2c2d6aec9dc87af83b3e5331d57c6282329d675e
Miguel Angel Vera Beltrán <miguel-angel.vera-beltran@outlook.com>
2023-04-13 15:33:23 -0600

    Se modifica MvcConfig para aputar al directorio uploads que esta en la raiz del proyecto

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -1,22 +1,32 @@
 package com.bolsadeideas.springboot.app;
 
+import java.nio.file.Paths;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer{
-
+	
+	//debug del resource path
+	private final Logger log= LoggerFactory.getLogger(getClass()); 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
 		WebMvcConfigurer.super.addResourceHandlers(registry);
+		
+		//toUri toma el path y agrega el esquema "file:/" y lo convierte a string
+		String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+		log.info(resourcePath); // pra visulaizar la ruta en consola
 		//registrar la nueva ruta con recursos estaticos
 		//agregamos doble * para mapear al nombre de la img y que esta se pueda cargar en ver.html,
 		//corresponden a un nombre variable de la img
 		// solo es la url que apunta al dir fisico
 		registry.addResourceHandler("/uploads/**")
-		.addResourceLocations("file:/C:/Temp/uploads/");//directorio fisico
+		.addResourceLocations(resourcePath);//contiene el uploads con la ruta absoluta con esquema file:/
 	}
 
 	
